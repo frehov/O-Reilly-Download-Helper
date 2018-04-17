181759d4c8ed5c732b411fda0f2a112ab417d3c4
Fredrik Hov <fredrik.hov@knowit.no>
2018-04-17 09:16:28 +0200

    cleaned up old unused code

--- download.js
+++ download.js
@@ -1,33 +1,14 @@
 var pageURL = location.href.replace(/csp.*/, "csp?download=true");
-var downloadURL = pageURL.replace(/\/free\//, "/free/files/").replace(/csp.*/, "");
 
-frmBtn = document.getElementById("x_a")
-
-frmBtn.addEventListener("click", function () {
+document.getElementById("x_a").addEventListener("click", function () {
     createCookie("DownloadFormFilled", true, 1);
 });
 
-//dlBtn = "<button type=\"submit\" id=\"x_b\" name=\"x-b\" value=\"Download Your Free Ebook\" class=\"btn\" onclick=\"window.location=\'"+ encodeURIComponent(pageURL) +"\';\">Download the free ebook</button>"
-dlBtn = "<a href='" + pageURL + "' class='btn'>Go to Download</a>"
+dlBtn = "<a href='" + pageURL + "' class='btn'>Go to Download</a>";
 
 if(readCookie("DownloadFormFilled") === "true") {
-    downloadHeader = $(".download-form > .fwb").text("Download the eBook");
-    var dlDiv = $(".download-form #download_requests_form");
-    //dlDiv.empty();
-    //dlDiv.append(downloadHeader);
-    dlDiv.append(dlBtn);
-
-/*
-    $.get(pageURL, null, function(text) {
-        var downloadField = $(text).find(".download-links");
-        dlDiv.append(downloadField);
-
-        var buttons = $.find("a[id*='download--']");
-        for(i = 0; i < buttons.length; i++) {
-            $(buttons[i]).attr("href", downloadURL.concat(buttons[i].childNodes[1].textContent.toLowerCase()));
-        }
-    });
-*/
+    $(".download-form > .fwb").text("Download the eBook");
+    $(".download-form #download_requests_form").append(dlBtn);
 }
 
 function createCookie(name,value,days) {
